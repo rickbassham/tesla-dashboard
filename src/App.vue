8952ac0c34fc6ff7545ad1115d8ce059368e1952
Rick Bassham <brodrick.bassham@gmail.com>
2020-01-01 13:04:52 -0500

    add switch for dark mode

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,26 @@
 <template>
-  <v-app id="inspire">
+  <v-app id="inspire" :dark="setTheme">
     <v-app-bar app>
       <v-toolbar-title>Tesla Dashboard</v-toolbar-title>
       <v-spacer></v-spacer>
-      <v-btn class="mx-2" to="/"><v-icon left>mdi-home</v-icon>Home</v-btn>
-      <v-btn class="mx-2" to="/guest"><v-icon left>mdi-account</v-icon>Guest Instructions</v-btn>
-      <v-btn class="mx-2" to="/abrp"><v-icon left>mdi-map</v-icon>A Better Route Planner</v-btn>
-      <v-btn class="mx-2" to="/waze"><v-icon left>mdi-map</v-icon>Waze</v-btn>
-      <v-btn class="mx-2" to="/plugshare"><v-icon left>mdi-power-plug</v-icon>PlugShare</v-btn>
-      <v-btn class="mx-2" to="/setup"><v-icon left>mdi-settings</v-icon>Settings</v-btn>
+      <v-btn class="mx-2" to="/">
+        <v-icon left>mdi-home</v-icon>Home
+      </v-btn>
+      <v-btn class="mx-2" to="/guest">
+        <v-icon left>mdi-account</v-icon>Guest Instructions
+      </v-btn>
+      <v-btn class="mx-2" to="/abrp">
+        <v-icon left>mdi-map</v-icon>A Better Route Planner
+      </v-btn>
+      <v-btn class="mx-2" to="/waze">
+        <v-icon left>mdi-map</v-icon>Waze
+      </v-btn>
+      <v-btn class="mx-2" to="/plugshare">
+        <v-icon left>mdi-power-plug</v-icon>PlugShare
+      </v-btn>
+      <v-btn class="mx-2" to="/setup">
+        <v-icon left>mdi-settings</v-icon>Settings
+      </v-btn>
     </v-app-bar>
 
     <v-content>
@@ -20,8 +32,11 @@
     </v-content>
     <v-footer app>
       <v-btn text>&copy; 2019 Rick Bassham</v-btn>
-      <v-btn text href="https://github.com/rickbassham/tesla-dashboard" target="_blank"><v-icon left>mdi-github-circle</v-icon>GitHub</v-btn>
+      <v-btn text href="https://github.com/rickbassham/tesla-dashboard" target="_blank">
+        <v-icon left>mdi-github-circle</v-icon>GitHub
+      </v-btn>
       <v-spacer></v-spacer>
+      <v-switch :label="`Dark Theme`" v-model="goDark"></v-switch>
       <v-btn text href="https://ts.la/brodrick17858">Tesla Referral Link</v-btn>
     </v-footer>
   </v-app>
@@ -29,13 +44,23 @@
 
 <script>
 export default {
-  components: {
-  },
+  components: {},
   props: {
     source: String
   },
   data: () => ({
-    drawer: false,
+    goDark: false
   }),
+  computed: {
+    setTheme() {
+      if (this.goDark == true) {
+        this.$vuetify.theme.dark = true; // eslint-disable-line vue/no-side-effects-in-computed-properties
+        return true;
+      } else {
+        this.$vuetify.theme.dark = false; // eslint-disable-line vue/no-side-effects-in-computed-properties
+        return false;
+      }
+    }
+  }
 };
 </script>
