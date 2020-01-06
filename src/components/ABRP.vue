d46c392ba86907d69765865a4491d99beffde904
Rick Bassham <brodrick.bassham@gmail.com>
2020-01-06 17:00:51 -0500

    style: reformat abrp

--- src/components/ABRP.vue
+++ src/components/ABRP.vue
@@ -1,3 +1,8 @@
 <template>
-  <iframe src="https://new.abetterrouteplanner.com/" width="100%" height="100%" style="border: 0;" />
+  <iframe
+    src="https://new.abetterrouteplanner.com/"
+    width="100%"
+    height="100%"
+    style="border: 0;"
+  />
 </template>
