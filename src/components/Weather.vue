cb183445ca242ab2d54dcdb2c30da2ace20651c9
Rick Bassham <brodrick.bassham@gmail.com>
2020-01-06 17:02:08 -0500

    style: reformat weather

--- src/components/Weather.vue
+++ src/components/Weather.vue
@@ -8,36 +8,36 @@ const uuidv4 = require("uuid/v4");
 
 export default {
   props: {
-    location: Object,
+    location: Object
   },
   watch: {
     location: function() {
-      this.updateMap()
-    },
+      this.updateMap();
+    }
   },
   methods: {
     updateMap() {
       if (this.location && this.location.latitude && this.location.longitude) {
         const el = document.getElementById(this.uuid);
         if (el.lastChild) {
-          el.lastChild.remove()
+          el.lastChild.remove();
         }
 
         const weatherURL =
-            '<script type="text/javascript" src="https://darksky.net/widget/graph-bar/' +
-            this.location.latitude +
-            "," +
-            this.location.longitude +
-            '/us12/en.js?width=100%&height=400&title=Full Forecast&bgColor=transparent&transparency=true&fontFamily=Default&currentDetailsOption=true"></scri' +
-            "pt>";
+          '<script type="text/javascript" src="https://darksky.net/widget/graph-bar/' +
+          this.location.latitude +
+          "," +
+          this.location.longitude +
+          '/us12/en.js?width=100%&height=400&title=Full Forecast&bgColor=transparent&transparency=true&fontFamily=Default&currentDetailsOption=true"></scri' +
+          "pt>";
         postscribe("#" + this.uuid, weatherURL);
       }
-    },
+    }
   },
   data() {
     return {
-      uuid: uuidv4(),
+      uuid: uuidv4()
     };
-  },
+  }
 };
 </script>
