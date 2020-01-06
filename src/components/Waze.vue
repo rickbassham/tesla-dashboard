9348d09a23586a2bd7e27400082281bcffe3e8cc
Rick Bassham <brodrick.bassham@gmail.com>
2020-01-06 17:03:04 -0500

    style: reformat waze

--- src/components/Waze.vue
+++ src/components/Waze.vue
@@ -1,3 +1,8 @@
 <template>
-  <iframe src="https://teslawaze.azurewebsites.net/" width="100%" height="100%" style="border: 0;" />
+  <iframe
+    src="https://teslawaze.azurewebsites.net/"
+    width="100%"
+    height="100%"
+    style="border: 0;"
+  />
 </template>
